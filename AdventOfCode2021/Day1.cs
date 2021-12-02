748680f55964c87b8f1904fab445e448fe37159c
Cherie Deng <cheriedengyiyi@gmail.com>
2021-12-03 00:42:07 +1300

    Setup.

---
+++ AdventOfCode2021/Day1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Day1
+{
+    public class Day1Part1
+    {
+        public int CountLargerMeasurements()
+        {
+            return 1;
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day1;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var part1 = new Day1Part1();
+            Console.WriteLine($"Day1Part1 result: {part1.CountLargerMeasurements()}");
+        }
+    }
+}
